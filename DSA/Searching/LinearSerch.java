bff48be48336d90da3962141f9b0d555e63e1b64
Santu-kumar364 <santukumar7619@gmail.com>
2025-01-10 17:17:08 +0530

    add files

---
+++ DSA/Searching/LinearSerch.java
@@ -0,0 +1,8 @@
+package Java_Bootcamp.DSA.Searching;
+
+public class LinearSerch {
+    public static void main(String[] args) {
+        System.out.println("Lines");
+    }
+    
+}
---
+++ OOPS/basic.java
@@ -0,0 +1,8 @@
+package Java_Bootcamp.OOPS;
+
+public class basic {
+    public static void main(String[] args) {
+        System.out.println("hello everyone");
+    }
+    
+}
