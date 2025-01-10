66e30448062809e82e5dbbb089e554509783430c
Santu-kumar364 <santukumar7619@gmail.com>
2025-01-10 18:23:44 +0530

    add

--- OOPS/basic.java
+++ OOPS/basic.java
@@ -2,7 +2,49 @@ package Java_Bootcamp.OOPS;
 
 public class basic {
     public static void main(String[] args) {
-        System.out.println("hello everyone");
+
+        // store 5 numbers
+        //int[] num = new int[5];
+
+        // store 5 names
+        //String[] name = new String[5];
+
+        // to store data of 5 students(including name, roll_number, marks)
+        Students[] students = new Students[5];
+
+        students[0] = new Students(578, "santu", 80.8f);
+        
+        students[1] = new Students(143, "Ajeet", 72.4f);
+        
+        students[2] = new Students(123, "Anju", 65.8f);
+        
+        students[3] = new Students(58, "Gupta", 50.8f);
+        
+        students[4] = new Students(78, "kumar", 62f);
+
+        for(Students s : students) {
+            System.out.println(s);
+        }
+       
+        
     }
     
+}
+
+class Students {
+    int roll_number;
+    String name;
+    Float marks;
+
+    Students (int roll_number, String name, Float marks) {
+        this.roll_number = roll_number;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public String toString() {
+        return "Name: " + name + ", Roll Number: " + roll_number + ", Marks: " + marks;
+    }
+
+  
 }
